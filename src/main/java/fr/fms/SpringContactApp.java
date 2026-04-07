32c7b336e46c225d7609e883232d9ac2b052dfa5
DCluseau <daphne.cluseau@fms-ea.com>
2026-04-07 11:03:12 +0200

    Implemented User, Contact and Category entities

---
+++ src/main/java/fr/fms/SpringContactApp.java
@@ -0,0 +1,37 @@
+package fr.fms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import fr.fms.dao.CategoryRepository;
+import fr.fms.dao.ContactRepository;
+import fr.fms.dao.UserRepository;
+import fr.fms.entities.Category;
+import fr.fms.entities.Contact;
+import fr.fms.entities.User;
+
+@SpringBootApplication
+public class SpringContactApp implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContactApp.class, args);
+	}
+	
+	public void run(String...args) throws Exception{
+//		User user = new User("Toto", "pwdfre", "fnjsofrd@fnfvso.jfi");
+//		Category category = new Category("Work");
+//		userRepository.save(user);
+//		categoryRepository.save(category);
+//		contactRepository.save(new Contact("Tata", "b.b@b.b", user, category));
+	}
+}
---
+++ src/main/java/fr/fms/entities/Category.java
@@ -0,0 +1,37 @@
+package fr.fms.entities;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class Category implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@NotNull
+	private String name;
+	@OneToMany
+	private List<Contact> contacts;
+	
+	public Category(String name) {
+		this.name = name;
+	}
+}
